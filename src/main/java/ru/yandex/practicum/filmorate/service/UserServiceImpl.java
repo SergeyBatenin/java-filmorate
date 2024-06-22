5eb0db2fc72615e7905cc1a0e6605c91fd573ea4
grenadinio <grenadinio@yandex.ru>
2024-06-22 18:08:43 +0300

    code review

--- src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
@@ -22,7 +22,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getById(long userId) {
-        return repository.getById(userId).orElseThrow(() -> new NotFoundException("Пользователь с id " + userId + " не найден."));
+        return repository.getById(userId)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id " + userId + " не найден."));
     }
 
     @Override
@@ -33,11 +34,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User update(User user) {
-        final User updatedUser = repository.getById(user.getId())
-                .orElseThrow(() -> {
-                    log.debug("UPDATE {}. Пользователь с айди {} не найден", user, user.getId());
-                    return new NotFoundException("Пользователь с id=" + user.getId() + " не существует");
-                });
+        checkUserExistence(user.getId(),"UPDATE");
         checkAndInitializeUserName(user);
         return repository.update(user);
     }
@@ -49,56 +46,28 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void addFriend(long userId, long friendId) {
-        final User user = repository.getById(userId)
-                .orElseThrow(() -> {
-                    log.debug("ADD-FRIEND {}<->{}. Пользователь с айди {} не найден", userId, friendId, userId);
-                    return new NotFoundException("Пользователь с id=" + userId + " не существует");
-                });
-        final User friend = repository.getById(friendId)
-                .orElseThrow(() -> {
-                    log.debug("ADD-FRIEND {}<->{}. Пользователь с айди {} не найден", userId, friendId, friendId);
-                    return new NotFoundException("Пользователь с id=" + friendId + " не существует");
-                });
+        checkUserExistence(userId,"ADD-FRIEND-USER");
+        checkUserExistence(friendId,"ADD-FRIEND-FRIEND");
         repository.addFriend(userId, friendId);
     }
 
     @Override
     public void deleteFriend(long userId, long friendId) {
-        User user = repository.getById(userId)
-                .orElseThrow(() -> {
-                    log.debug("ADD-FRIEND {}<->{}. Пользователь с айди {} не найден", userId, friendId, userId);
-                    return new NotFoundException("Пользователь с id=" + userId + " не существует");
-                });
-        User friend = repository.getById(friendId)
-                .orElseThrow(() -> {
-                    log.debug("ADD-FRIEND {}<->{}. Пользователь с айди {} не найден", userId, friendId, friendId);
-                    return new NotFoundException("Пользователь с id=" + friendId + " не существует");
-                });
+        checkUserExistence(userId,"DELETE-FRIEND-USER");
+        checkUserExistence(friendId,"DElETE-FRIEND-FRIEND");
         repository.deleteFriend(userId, friendId);
     }
 
     @Override
     public Collection<User> getFriends(long userId) {
-        final User user = repository.getById(userId)
-                .orElseThrow(() -> {
-                    log.debug("GET-FRIENDS {}. Пользователь с айди {} не найден", userId, userId);
-                    return new NotFoundException("Пользователь с id=" + userId + " не существует");
-                });
+        checkUserExistence(userId,"GET-FRIENDS");
         return repository.getFriends(userId);
     }
 
     @Override
     public Collection<User> getCommonFriends(long userId, long otherId) {
-        final User user = repository.getById(userId)
-                .orElseThrow(() -> {
-                    log.info("GET-COMMON-FRIENDS {}<->{}. Пользователь с айди {} не найден", userId, otherId, userId);
-                    return new NotFoundException("Пользователь с id=" + userId + " не существует");
-                });
-        final User other = repository.getById(otherId)
-                .orElseThrow(() -> {
-                    log.info("GET-COMMON-FRIENDS {}<->{}. Пользователь с айди {} не найден", userId, otherId, otherId);
-                    return new NotFoundException("Пользователь с id=" + otherId + " не существует");
-                });
+        checkUserExistence(userId,"GET-COMMON-FRIENDS-USER");
+        checkUserExistence(otherId,"GET-COMMON-FRIENDS-FRIEND");
         return repository.getCommonFriends(userId, otherId);
     }
 
@@ -106,5 +75,13 @@ public class UserServiceImpl implements UserService {
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
         }
+    }
+
+    private void checkUserExistence(Long userId, String method){
+        repository.getById(userId)
+                .orElseThrow(() -> {
+                    log.info("{} Пользователь с id={} не найден", method, userId);
+                    return new NotFoundException("Пользователя с id=" + userId + " не существует");
+                });
     }
 }
