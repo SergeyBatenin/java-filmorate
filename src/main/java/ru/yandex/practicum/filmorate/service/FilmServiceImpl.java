4ad03206f40d09220f62eb93e59d944800d62bc6
Ivan498 <vankar2824@mail.ru>
2024-06-24 12:10:24 +0300

    fixed the errors num 4

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
@@ -19,7 +19,6 @@ import ru.yandex.practicum.filmorate.repository.user.UserRepository;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
