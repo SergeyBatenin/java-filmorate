170dc01446662774444549815d90d3ca48a613b8
Sergey Batenin <inferno.87@mail.ru>
2024-06-24 16:21:06 +0300

    fix director update logging

--- src/main/java/ru/yandex/practicum/filmorate/service/DirectorServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/DirectorServiceImpl.java
@@ -38,7 +38,7 @@ public class DirectorServiceImpl implements DirectorService {
     public Director update(Director director) {
         directorRepository.getById(director.getId())
                 .orElseThrow(() -> {
-                    log.debug("GET DIRECTOR {}. Режиссер с айди {} не найден", director.getId(), director.getId());
+                    log.debug("UPDATE DIRECTOR {}. Режиссер с айди {} не найден", director.getId(), director.getId());
                     return new NotFoundException("Режиссер с id=" + director.getId() + " не существует");
                 });
 
